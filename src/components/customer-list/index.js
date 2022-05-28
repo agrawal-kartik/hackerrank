55c343e40e4d8f26bf10eb15111c6c595901cf66
Admin <admin.push@hackerrank.workspace>
2022-05-28 10:38:21 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/components/customer-list/index.js
+++ src/components/customer-list/index.js
@@ -1,16 +1,44 @@
-import React  from "react";
+import React, {useState}  from "react";
 import "./index.css";
 
 function CustomerList() {
+  const[name,setName]=useState("");
+  const[heading,setHeading]=useState("");
+  function handleChange(event){
+    setName(event.target.value);
+  }
+  function handleClick(){
+    setHeading(name);
+  }
   return (
     <div className="mt-75 layout-column justify-content-center align-items-center">
       <section className="layout-row align-items-center justify-content-center">
-        <input type="text" className="large" placeholder="Name" data-testid="app-input"/>
-        <button type="submit" className="ml-30" data-testid="submit-button">Add Customer</button>
+        <input 
+        onChange={handleChange}
+        value={name}
+        type="text" 
+        className="large" 
+        placeholder="Name" 
+        data-testid="app-input"
+        />
+        <button 
+         onChange={handleChange}
+        type="submit" 
+        className="ml-30" 
+        data-testid="submit-button"
+        >
+          Add Customer
+          </button>
       </section>
 
       <ul className="styled mt-50" data-testid="customer-list">
-          <li className="slide-up-fade-in" data-testid="list-item1" key="list-item1" >Customer</li>
+          <li 
+          className="slide-up-fade-in" 
+          data-testid="list-item1" 
+          key="list-item1" 
+          >Customer
+          <h1>{heading}</h1>
+          </li>
       </ul>
     </div>
   );
